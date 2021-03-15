053f45f6a35f73d98daa8667db510c3e82a2043c
VicDev13 <victor.pinheiro@aluno.faculdadeimpacta.com.br>
2021-03-15 19:56:45 -0300

    Correção do login e senha, inclusão de imagem na tela Grifinoria

--- app/src/main/java/br/com/ddm/hogwartshistorys/MainActivity.kt
+++ app/src/main/java/br/com/ddm/hogwartshistorys/MainActivity.kt
@@ -14,16 +14,17 @@ class MainActivity : AppCompatActivity() {
         login_imagem.setImageResource(R.drawable.imagem_login)
 
         login_botao.setOnClickListener {
-            val nome_usuario = campo_usuario.text.toString()
-            val senha_usuario = campo_usuario.text.toString()
-            if (nome_usuario == "aluno" && senha_usuario == "impacta") {
-                val intent = Intent(this, TelaInicialActivity::class.java)
-                startActivity(intent)
-            } else {
-                Toast.makeText(this, "Usuário ou senha incorretos", Toast.LENGTH_SHORT).show()
-            }
-//            val intent = Intent(this, TelaInicialActivity::class.java)
-//            startActivity(intent)
+//            val nome_usuario = campo_usuario.text.toString()
+//            val senha_usuario = campo_senha.text.toString()
+//            if (nome_usuario == "aluno" && senha_usuario == "impacta") {
+//                val intent = Intent(this, TelaInicialActivity::class.java)
+//                startActivity(intent)
+//            } else {
+//                Toast.makeText(this, "Usuário ou senha incorretos", Toast.LENGTH_SHORT).show()
+//            }
+
+            val intent = Intent(this, TelaInicialActivity::class.java)
+            startActivity(intent)
         }
 
     }
