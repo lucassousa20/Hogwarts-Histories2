a6c85a3e9b5729ea5797299837bb8901c19a5f86
lucassousa20 <60761607+lucassousa20@users.noreply.github.com>
2021-06-08 21:08:59 -0300

    att

---
+++ app/src/main/java/br/com/ddm/hogwartshistories/Disciplina.kt
@@ -0,0 +1,4 @@
+package br.com.ddm.hogwartshistories
+
+class Disciplina {
+}
--- app/src/main/java/br/com/ddm/hogwartshistories/TelaInicialActivity.kt
+++ app/src/main/java/br/com/ddm/hogwartshistories/TelaInicialActivity.kt
@@ -5,17 +5,24 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
 import android.widget.SearchView
 import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
 import androidx.core.view.isVisible
+import com.google.android.material.navigation.NavigationView
 import kotlinx.android.synthetic.*
 import kotlinx.android.synthetic.main.activity_tela_inicial.*
 import kotlinx.android.synthetic.main.toolbar.*
 
-class TelaInicialActivity : AppCompatActivity() {
+class TelaInicialActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    private val context: Context get() = this
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tela_inicial)
@@ -55,6 +62,39 @@ class TelaInicialActivity : AppCompatActivity() {
             intent.putExtras(params)
             startActivity(intent)
         }
+
+        configuraMenuLateral()
+    }
+
+    private fun configuraMenuLateral () {
+        var toogle = ActionBarDrawerToggle(
+            this,
+            layout_menu_lateral,
+            toolbar,
+            R.string.open,
+            R.string.closed
+        )
+
+        layout_menu_lateral.addDrawerListener(toogle)
+        toogle.syncState()
+
+        nav_menu_lateral.setNavigationItemSelectedListener(this)
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.nav_displiciplinas -> {
+                Toast.makeText(this, "Clicou Disciplicnas", Toast.LENGTH_SHORT).show()
+            }
+            R.id.nav_forum -> {
+                Toast.makeText(this, "Clicou Forum", Toast.LENGTH_SHORT).show()
+            }
+            R.id.nav_localizacao -> {
+                Toast.makeText(this, "Clicou Disciplicnas", Toast.LENGTH_SHORT).show()
+            }
+        }
+        layout_menu_lateral.closeDrawer(GravityCompat.START)
+        return true
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
